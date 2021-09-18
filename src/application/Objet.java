0fcbeb68211c27aad9d5618ce3a1c9a63408993c
julie pessey <julie.pessey@etu.u-paris.fr>
2021-09-18 17:49:00 +0200

    définition des classes Objet et sac a dos

--- src/application/Objet.java
+++ src/application/Objet.java
@@ -1,5 +1,31 @@
 package application;
 
 public class Objet {
+	
+	private String name;
+	private float weight;
+	private float value;
+	private boolean isInTheBag;
+	private float ratio;
+	
+	public Objet(String name, float weight, float value) {
+		this.name = name;
+		this.weight = weight;
+		this.value = value;
+		this.isInTheBag = false;
+		this.ratio = value/weight;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public float getValue() {
+		return value;
+	}
+	
 }
---
+++ src/application/SacADos.java
@@ -0,0 +1,76 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SacADos {
+	
+	private float maxWeight;
+	private ArrayList<Objet> objectsList;
+	
+	
+	//Construteur générant un sac vide
+	public SacADos() {
+		this.maxWeight = 0;
+		this.objectsList = new ArrayList<Objet>();
+	}
+	
+	public SacADos(String path, float maxWeight) {
+		this.maxWeight = maxWeight;
+		this.objectsList = new ArrayList<Objet>();
+		
+		String fileContent = readTextFile(path);
+		
+	}
+
+	//voir si on créer une nouvelle classe pour les méthodes suivantes 
+	private String readTextFile(String path) {
+		StringBuilder fileContent = new StringBuilder();
+		try {
+			File f = new File(path);
+			BufferedReader b = new BufferedReader(new FileReader(f));
+			String readLine = "";
+			
+			while ((readLine = b.readLine()) != null) {
+				fileContent.append(readLine + "\n");
+            }
+
+		} catch (IOException e) {
+	        e.printStackTrace();
+	    }
+		return fileContent.toString();
+		
+	}
+	
+	private Objet initObj(String obj) {
+		String[] objInfo = obj.split(";");
+		if (objInfo.length == 2) {
+			return new Objet (objInfo[0], Float.parseFloat(objInfo[1]), Float.parseFloat(objInfo[2]));
+		}
+		return null; // return exception obj non valide
+	}
+		
+
+			
+	private ArrayList initObjList(String url) {
+		String[] file = readTextFile(url).split("\n");
+		ArrayList<Objet> objectsList = new ArrayList<Objet>();
+		for (String obj : file) {
+			objectsList.add(initObj(obj));
+		}
+		return objectsList;
+		
+	}
+	
+	
+	public String toString() {
+		StringBuilder bagContent = new StringBuilder();
+		for(Objet o : objectsList) {
+			bagContent.append(o.getName() + " ; " + o.getWeight() + " ; " + o.getValue() + "\n");
+		}
+		return bagContent.toString();
+	}
+}
