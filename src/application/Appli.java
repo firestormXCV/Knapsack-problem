b19eb8b75f865687496fa207b1f2baa3a43b716e
TBrioche <72711057+TBrioche@users.noreply.github.com>
2021-10-17 21:21:00 +0200

    cas de l'erreur de saisie

--- src/application/Appli.java
+++ src/application/Appli.java
@@ -17,30 +17,41 @@ public class Appli {
 		System.out.println("Veuillez saisir le chemin du fichier, le poids maximal du sac ainsi que la méthode de résolution : ");
 		System.out.println("Exemple : items.txt 12 pse");
 		System.out.print(">");
-		saisie = sc.nextLine();
+		boolean loop = true;
+		while (loop ) {
+			try {
+				saisie = sc.nextLine();
+				
+				String[] saisietxt = saisie.split(" ");
 		
-		String[] saisietxt = saisie.split(" ");
-
-		SacADos bag = new SacADos(saisietxt[0], Float.parseFloat(saisietxt[1]));
-		
-		String res = saisietxt[2];
-		switch(res) {
-			case "glouton":
-				Glouton glouton = new Glouton();
-				bag.setObjectList(QuickSort.QuickSort(bag.getObjectList(),(float) 0, (float) bag.getObjectList().size() - 1));
-				glouton.resolution(bag);
-				System.out.println(bag.toStringIn());
-				break;
-			case "prog. dynamique":
-				progDynamique progDynamique = new progDynamique();
-				progDynamique.resolution(bag);
-				System.out.println(bag.toStringIn());
-				break;
-			case "pse":
-				PSE pse = new PSE();
-				pse.resolution(bag);
-				System.out.println(bag.toStringIn());
-				break;	
+				SacADos bag = new SacADos(saisietxt[0], Float.parseFloat(saisietxt[1]));
+				
+				String res = saisietxt[2];
+				switch(res) {
+					case "glouton":
+						Glouton glouton = new Glouton();
+						bag.setObjectList(QuickSort.QuickSort(bag.getObjectList(),(float) 0, (float) bag.getObjectList().size() - 1));
+						glouton.resolution(bag);
+						System.out.println(bag.toStringIn());
+						loop = false;
+						break;
+					case "prog. dynamique":
+						progDynamique progDynamique = new progDynamique();
+						progDynamique.resolution(bag);
+						System.out.println(bag.toStringIn());
+						loop = false;
+						break;
+					case "pse":
+						PSE pse = new PSE();
+						pse.resolution(bag);
+						System.out.println(bag.toStringIn());
+						loop = false;
+						break;	
+				}
+			} catch (Exception e) {
+				System.out.println("Saisie non valide merci de recommencer");
+			}
+			
 		}
 		
 //		SacADos sacGlouton = new SacADos("items.txt", 8);
